c347f95532cd5915f1e97f9d9abb79c9171eaca1
baekbsy1111-star <baekbsy1111@gmail.com>
2025-11-13 15:29:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap (int x, int y) { 
-int temp; 
-temp = x; 
-x = y; 
-y = temp; 
-} 
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
-void main(void) { 
-int a = 3; 
-int b = 5; 
-swap(a, b); 
-printf("a:%i, b:%i\n", a, b); 
+int main(void) {
+    int a = 3;
+    int b = 5;
+
+    swap(&a, &b);
+    printf("a: %i, b: %i\n", a, b);  
+
   
   system("PAUSE");	
   return 0;
